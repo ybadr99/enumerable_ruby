dca7c35826d4e8523f3a8d11152da1b1bd08baa6
ybadr99 <yousefbadr909@gmail.com>
2023-07-22 21:54:34 +0300

    remove unused code

--- main.rb
+++ main.rb
@@ -1,14 +1,12 @@
-# main.rb
 require_relative 'my_list'
 
-# Sample usage
 list = MyList.new(1, 2, 3, 4)
 
-puts(list.all? { |e| e < 5 }) # Should output: true
-puts(list.all? { |e| e > 5 }) # Should output: false
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 }) 
 
-puts(list.any? { |e| e == 2 }) # Should output: true
-puts(list.any? { |e| e == 5 }) # Should output: false
+puts(list.any? { |e| e == 2 }) 
+puts(list.any? { |e| e == 5 }) 
 
 filtered_list = list.filter(&:even?)
-puts filtered_list.inspect # Should output: [2, 4]
+puts filtered_list.inspect 
